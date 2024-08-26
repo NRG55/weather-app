41972451a96470733be66b656a691a63686d1a6c
NRG55 <maloj_1980@yahoo.fr>
2024-08-26 03:09:59 +0100

    getWeather.js - function getWeather (added a try catch error block). Index.html - added header, nav and form (with an input and a search button) elements.

--- src/modules/getWeather.js
+++ src/modules/getWeather.js
@@ -2,8 +2,13 @@ export const getWeather = async (location) => {
     const apiKey = 'b87c3740ed6b4c5db6522324242508';
     const url = `http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${location}`;
 
+    try {
     const response = await fetch(url, { mode: 'cors'});
     const data = await response.json();
 
     return data;
+
+    } catch (error) {
+        console.error('Fetch weather API is failed', error);
+    };
 }
